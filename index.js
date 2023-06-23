29b2cc61a6fb15c41e292a7d4aff29116633f651
saniyabgm <136998217+saniyabgm@users.noreply.github.com>
2023-06-23 11:06:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+   function redirectToURL(btnId){
+   	if(btnId=="button1")
+   		window.location.replace("https://saniyabgm.github.io/slider/");
+   	else if(btnId=="button2")
+   		window.location.replace("https://saniyabgm.github.io/game/");
+   }
+</script>
